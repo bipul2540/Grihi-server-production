9f671f7cbc7b21c6fb2b656331e514a4fbae78a0
bipul <bipulkumar2540@gmail.com>
2024-09-23 21:36:29 +0530

    db-config changes

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,12 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceAccount = require("./../../db-config.json");
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+};
 
 initializeApp({
   credential: cert(serviceAccount),
